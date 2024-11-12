69d7118ab4a80351ede78412c6bbf7fa6592df3a
aoconnor <oconnorails1@gmail.com>
2024-11-12 12:16:40 +0000

    Wrote about tasks 6 and 7 in weather.ipynb.

---
+++ athenry_weather.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+wget https://prodapi.metweb.ie/observations/athenry/today -O `date +"%Y%m%d_%H%M%S"`_athenry.json 
--- data/weather/athenry_weather.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-wget -O `date +"%Y%m%d_%H%M%S"`_athenry.json https://prodapi.metweb.ie/observations/athenry/today
