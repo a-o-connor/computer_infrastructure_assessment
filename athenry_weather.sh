2f7dd7dbb810759813fd348d97ff8e82114d06bc
a-o-connor <157632616+a-o-connor@users.noreply.github.com>
2024-11-22 14:17:15 +0000

    Changed athenry_weather.sh to save the athenry weather data into the data/weather folder instead of main root

--- athenry_weather.sh
+++ athenry_weather.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-wget https://prodapi.metweb.ie/observations/athenry/today -O `date +"%Y%m%d_%H%M%S"`_athenry.json 
+wget https://prodapi.metweb.ie/observations/athenry/today -O ./data/weather/`date +"%Y%m%d_%H%M%S"`_athenry.json 
